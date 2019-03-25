c2ad74ffbb1426ef373b45dbfff9f40741688cf8
iamroy <royrmagdaong@gmail.com>
2019-03-25 12:51:02 +0800

    add header,content,footer components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <div>
-    {{greeting}}
+    <app-header></app-header>
+    <app-content v-bind:students='students'></app-content>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Content from './components/Content'
+
+
 export default {
-  name: 'app',
+  components:{
+    'app-footer':Footer,
+    'app-header':Header,
+    'app-content':Content
+  },
   data () {
     return {
-      greeting: 'good morning'
+      students:[
+        {name:'Roy Magdaong', Course: 'BSIT', Specialization:'Programming', show:false},
+        {name:'Christly Tejano', Course: 'BSIT', Specialization:'Networking', show:false},
+        {name:'Alvin Domingo', Course: 'BSIT', Specialization:'General IT', show:false},
+        {name:'Jhon Daniel Catalan', Course: 'BSIT', Specialization:'Programming', show:false},
+        {name:'Sanjie Esmero', Course: 'BSIT', Specialization:'Computer Hardware', show:false}
+      ]
     }
   }
 }
 </script>
 
 <style>
+body{
+  margin: 0%;
+}
 </style>
---
+++ src/components/Content.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li v-for="student in students" 
+      @click="student.show=!student.show" 
+      :key="student.index"
+    >
+      <h2>{{student.name}}</h2>
+      <caption>{{student.Course}}</caption>
+      <h3 v-show="student.show">{{student.Specialization}}</h3>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props:{
+    students:{
+      type: Array
+    }
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
